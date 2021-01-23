1ca8b09ed354f4de7608e7799d3d26bacb582192
zuka mujirishvili <zmuji18@freeuni.edu.ge>
2021-01-23 10:33:04 +0400

    Implemented dunder methods

--- Source/binary_heap.py
+++ Source/binary_heap.py
@@ -3,10 +3,10 @@
 class BinaryHeap:
     """This is a binary heap data structure."""
 
-    def __init__(self, size=None, key=(lambda x: x), data=None):
-        """Creates a Binary heap of size max({size}, #elements in {data}).
+    __INIT_MAX_SIZE = 5
 
-        Either {size} or {data} must be specified.
+    def __init__(self, data=None, key=(lambda x: x)):
+        """Creates a Binary (min) heap from the elements in {data}.
 
         {key} is a function that returns the value that is used during 
         comparisons (by default it is the element itself). The < operator is used
@@ -15,23 +15,44 @@ class BinaryHeap:
         (optional) Will add all the elements in {data} to the heap. 
         {data} must be an iterable object. O(N), N = size of the heap"""
 
-        pass
+        self.__size = 0
+        self.__max_size = BinaryHeap.__INIT_MAX_SIZE
+        self.__key = key
+
+        self.__list = [None] + [None]*self.__max_size
+
+        if hasattr(data, "__iter__"):  
+            for elem in data:
+                self.push(elem)
+        elif data is not None:
+            raise TypeError("Passed data is not an iterable.")
 
     def __len__(self):
         """Returns the size of the heap. O(1)"""
-        pass
+        
+        return self.__size
 
     def __str__(self):
         """Returns a string representation of the heap. O(N), N = size of the heap"""
-        pass
+        
+        return str(self.__list[1:(self.__size + 1)])
 
     def __iter__(self):
         """Returns an iterator for the elements in the heap. O(1)"""
-        pass
+        
+        return self
+
+    def __next__(self):
+        """Returns the next element in the iterator."""
+
+        if self:
+            return self.pop()
+        else:
+            raise StopIteration
 
     def push(self, element):
         """Adds {element} to the heap. O(logN), N = size of the heap"""
-        pass
+        pass 
 
     def pop(self):
         """Removes and returns the first element in the heap. O(logN), N = size of the heap"""
