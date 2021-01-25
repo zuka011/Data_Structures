7dc8323642660a67a7670af6f53e501d1958a12d
zuka mujirishvili <zmuji18@freeuni.edu.ge>
2021-01-25 12:49:51 +0400

    Finished implementing and testing union find (hopefully).

--- Source/union_find.py
+++ Source/union_find.py
@@ -1,13 +1,17 @@
 
 
+from typing import Iterable, Iterator
+
+
 class UnionFind:
     """This is a union find data structure."""
 
-    def __init__(self, size=None, data=None):   
+    def __init__(self, size: int=0, data: Iterable=None):   
         """Constructor for the union find structure.
 
-        This structure creates a bijection between it's elements and
-        numbers in the range [1, #elements in union find].
+        This structure creates a bijection between it's elements and 
+        numbers (initial groups) in the range [1, #elements in union find].
+        Later these indices can be used to operate the union find structure.
 
         example:
             u_find = UnionFind(["cat", "dog", 1, (2, 3)])
@@ -18,52 +22,108 @@ class UnionFind:
             
         size - if specified, will create a union find structure of size {size}
 
-        data - if specified, will create a union find structure from the elements in {data}
+        data - if specified, will create a union find structure from the elements in {data}.
+        {data} must be an iterable object.
 
-        if both are specified and {size} is less than #elements in data, it will be ignored,
+        if both are specified and {size} is less than #elements in {data}, it will be ignored,
         otherwise the remaining indices will be filled with None values. 
         O(N), N = #elements in union find"""
 
-        raise NotImplementedError
+        if data is None:
+            self.__list = [None] + [[None, i] for i in range(1, size+1)]
+        elif hasattr(data, "__iter__"):              
+            self.__list = [None] + [[data[i], i+1] for i in range(len(data))] + [[None, len(data)+1+i] for i in range(size - len(data))]
+        else:
+            raise TypeError("Passed data is not an iterable.")
 
-    def __len__(self):
-        """Returns the size of the union find structure. O(1)"""
+        self.__groups = len(self.__list) - 1
 
-        raise NotImplementedError
+    def __len__(self) -> int:
+        """Returns the number of groups in the union find structure. O(1)"""
 
-    def __str__(self):
+        return self.__groups
+
+    def __str__(self) -> str:
         """Returns a string representation of the union find
         in the form (element, group number). The group number
-        may be any number. O(N), N = #elements in union find"""
+        may be any number, but will be the same for elements
+        that belong to the same group. O(N), N = #elements in union find"""
 
-        raise NotImplementedError
+        return str([tuple(pair) for pair in self.__list[1:]])
 
-    def __iter__(self):
+    def __iter__(self) -> Iterator:
         """Returns an iterator to pairs of data in the form
         (element, group number), for all elements in the 
         union find. O(N), N = #elements in union find"""
 
-        raise NotImplementedError
+        return iter([tuple(pair) for pair in self.__list[1:]])
 
-    def add_element(self, element):
+    def add_element(self, element)  -> None:
         """Adds an element to the union find structure. O(1)"""
 
-        raise NotImplementedError
+        self.__list.append([element, len(self.__list)])
+        self.__groups += 1
 
-    def set_element(self, index, element):
+    def set_element(self, index: int, element) -> None:
         """Associates the number {index} with {element}. O(1)"""
 
-        raise NotImplementedError
+        if index < 1 or index > len(self.__list):
+            raise IndexError("No such index in union find.")
 
-    def same_group(self, index_1, index_2):
+        if index == len(self.__list):
+            self.add_element(element)
+        else:
+            self.__list[index][0] = element
+
+    def is_same_group(self, index_1: int, index_2: int) -> bool:
         """Returns True if element at {index_1} is in the same 
         group as the element at {index_2}. (Amortized) O(1)"""
 
-        raise NotImplementedError
+        self.__check_bounds(index_1)
+        self.__check_bounds(index_2)
+        
+        root_1 = self.__find_root(index_1)
+        root_2 = self.__find_root(index_2)
 
-    def merge(self, index_1, index_2):
+        return root_1 == root_2
+
+    def merge(self, index_1: int, index_2: int) -> None:
         """Merges the groups of the element at {index_1} and the 
         element at {index_2}. (Amortized) O(1)"""
 
-        raise NotImplementedError
+        root_1 = self.__find_root(index_1)
+        root_2 = self.__find_root(index_2)
+
+        if root_1 != root_2:
+            self.__groups -= 1
+
+        if root_1 < root_2:
+            self.__list[root_2][1] = root_1
+        else:
+            self.__list[root_1][1] = root_2
+
+    def __check_bounds(self, index: int):
+        """Checks to see if {index} is within the union
+        find structures bounds e.i. in the range [1, #elements in the union find]."""
+
+        if index < 1 or index >= len(self.__list):
+            raise IndexError("No such index in union find.")
+
+    def __find_root(self, index: int):
+        """Finds and returns the index of the root for the
+        group which element at {index} belongs to. This method
+        implements path compression for the union find structure."""
+
+        passed_indices = []
+
+        while self.__list[index][1] != index:
+            passed_indices.append(index)
+            index = self.__list[index][1]
+
+        for passed_index in passed_indices:
+            self.__list[passed_index][1] = index
+
+        return index
+
+
 
--- Tests/test_union_find.py
+++ Tests/test_union_find.py
@@ -3,6 +3,7 @@ import sys
 sys.path.append(r"..")
 
 import unittest
+from random import randint
 
 from Source.union_find import UnionFind
 
@@ -15,7 +16,7 @@ class TestUnionFind(unittest.TestCase):
         u_find = UnionFind(10)
         u_find = UnionFind(3, [1, 2, 3, 4, 5])
         u_find = UnionFind(10, [1, 2, 3, 4, 5])
-        u_find = UnionFind(["cat", "dog", [1, 2]])
+        u_find = UnionFind(data=["cat", "dog", [1, 2]])
 
     def test_len(self):
 
@@ -31,7 +32,7 @@ class TestUnionFind(unittest.TestCase):
         u_find = UnionFind(10, [1, 2, 3, 4, 5])
         self.assertEqual(len(u_find), 10)
 
-        u_find = UnionFind(["cat", "dog", [1, 2]])
+        u_find = UnionFind(data=["cat", "dog", [1, 2]])
         self.assertEqual(len(u_find), 3)
 
     def test_str(self):
@@ -48,13 +49,224 @@ class TestUnionFind(unittest.TestCase):
         u_find = UnionFind(10, [1, 2, 3, 4, 5])
         self.assertEqual(str(u_find), str([(i, i) for i in range(1, 6)] + [(None, i) for i in range(6, 11)]))
 
-        u_find = UnionFind(["cat", "dog", [1, 2]])
+        u_find = UnionFind(data=["cat", "dog", [1, 2]])
         self.assertEqual(str(u_find), "[('cat', 1), ('dog', 2), ([1, 2], 3)]")
 
     def test_iter(self):
 
-        pass
-    
+        u_find = UnionFind()
+        working_values = [pair for pair in u_find]
+        self.assertEqual(working_values, [])
+        
+        u_find = UnionFind(10)
+        working_values = [pair for pair in u_find]
+        self.assertEqual(working_values, [(None, i) for i in range(1, 11)])
+
+        u_find = UnionFind(3, [1, 2, 3, 4, 5])
+        working_values = [pair for pair in u_find]
+        self.assertEqual(working_values, [(i, i) for i in range(1, 6)])
+
+        u_find = UnionFind(10, [1, 2, 3, 4, 5])
+        working_values = [pair for pair in u_find]
+        self.assertEqual(working_values, [(i, i) for i in range(1, 6)] + [(None, i) for i in range(6, 11)])
+
+        u_find = UnionFind(data=["cat", "dog", [1, 2]])
+        working_values = [pair for pair in u_find]
+        self.assertEqual(working_values, [('cat', 1), ('dog', 2), ([1, 2], 3)])
+
+    def test_add_element(self):
+        
+        u_find = UnionFind()
+        data = [randint(0, 100) for i in range(50)]
+        self.__add_elems(u_find, data)
+
+        u_find = UnionFind(10)
+        data = [randint(0, 100) for i in range(50)]
+        self.__add_elems(u_find, data)
+        self.assertEqual([pair[0] for pair in u_find][:10], [None] * 10)
+
+        u_find = UnionFind(3, [1, 2, 3, 4, 5])
+        data = [randint(0, 100) for i in range(50)]
+        self.__add_elems(u_find, data)
+
+        u_find = UnionFind(10, [1, 2, 3, 4, 5])
+        data = [randint(0, 100) for i in range(50)]
+        self.__add_elems(u_find, data)
+
+        u_find = UnionFind(data=["cat", "dog", [1, 2]])
+        data = [randint(0, 100) for i in range(50)]
+        self.__add_elems(u_find, data)
+        self.__add_elems(u_find, data)
+
+    def test_set_element(self):
+
+        u_find = UnionFind(100)
+        data_pairs = [(i, randint(0, 300)) for i in range(1, 101)]
+        self.__set_elems(u_find, data_pairs)
+
+        u_find = UnionFind(100, [None] * 10)
+        data_pairs = [(i, randint(0, 300)) for i in range(11, 101)]
+        self.__set_elems(u_find, data_pairs)
+        
+        u_find = UnionFind(3, [1, 2, 3, 4, 5])
+        data_pairs = [(i, randint(0, 300)) for i in range(6, 101)]
+        self.__set_elems(u_find, data_pairs)
+
+        with self.assertRaises(IndexError):
+            
+            u_find = UnionFind(10)
+            u_find.set_element(12, None)
+
+        with self.assertRaises(IndexError):
+            
+            u_find = UnionFind(10)
+            u_find.set_element(0, None)
+
+    def test_merge(self):
+        
+        u_find = UnionFind(data=[randint(0, 100) for i in range(1, 11)])
+        
+        # Merge all odd indices together
+        self.__merge_into_group(u_find, [i for i in range(1, 11, 2)])
+
+        # Merge all even indices together
+        self.__merge_into_group(u_find, [i for i in range(2, 11, 2)])
+
+        # Merge the above two groups
+        self.__merge_pairs(u_find, [(1, 2)])
+        self.assertEqual(len(u_find), 1)
+
+        # Manual Test
+        u_find = UnionFind(data=[randint(0, 100) for i in range(1, 11)])
+
+        self.__merge_pairs(u_find, [(1, 3), (5, 7), (2, 4), (6, 8)])
+        self.assertEqual(len(u_find), 6)
+
+        self.__merge_pairs(u_find, [(9, 10)])
+        self.assertEqual(len(u_find), 5)
+
+        self.__merge_pairs(u_find, [(1, 5), (2, 6)])
+        self.assertEqual(len(u_find), 3)
+
+        self.__merge_pairs(u_find, [(7, 3), (8, 4)])
+        self.assertEqual(len(u_find), 3)
+
+        self.__merge_pairs(u_find, [(7, 2)])
+        self.assertEqual(len(u_find), 2)
+
+        self.__merge_pairs(u_find, [(8, 1)])
+        self.assertEqual(len(u_find), 2)
+
+        self.__merge_pairs(u_find, [(10, 2)])
+        self.assertEqual(len(u_find), 1)
+        
+    def test_is_same_group(self):
+        
+        u_find = UnionFind(data=[randint(0, 100) for i in range(1, 11)])
+        self.assertEqual(self.__check_if_elems_in_group(u_find, [i for i in range(1, 11)]), False)
+        
+        # Merge all odd indices together (followed by a check)
+        self.assertEqual(self.__check_if_elems_in_group(u_find, [i for i in range(1, 11, 2)]), False)
+        self.__merge_into_group(u_find, [i for i in range(1, 11, 2)])
+        self.assertEqual(self.__check_if_elems_in_group(u_find, [i for i in range(1, 11, 2)]), True)
+
+        # Merge all even indices together (followed by a check)
+        self.assertEqual(self.__check_if_elems_in_group(u_find, [i for i in range(2, 11, 2)]), False)
+        self.__merge_into_group(u_find, [i for i in range(2, 11, 2)])
+        self.assertEqual(self.__check_if_elems_in_group(u_find, [i for i in range(2, 11, 2)]), True)
+
+        # Merge the above two groups (followed by a check)
+        self.__merge_pairs(u_find, [(1, 2)])
+        self.assertEqual(len(u_find), 1)
+        self.assertEqual(self.__check_if_elems_in_group(u_find, [i for i in range(1, 11)]), True)
+
+        # Manual Test (followed by checks)
+        u_find = UnionFind(data=[randint(0, 100) for i in range(1, 11)])
+        self.assertEqual(self.__check_if_elems_in_group(u_find, [i for i in range(1, 11)]), False)
+
+        self.__merge_pairs(u_find, [(1, 3), (5, 7), (2, 4), (6, 8)])
+        self.assertEqual(len(u_find), 6)
+        self.__check_pairs(u_find, [(1, 3), (5, 7), (2, 4), (6, 8)])
+
+        self.__merge_pairs(u_find, [(9, 10)])
+        self.assertEqual(len(u_find), 5)
+        self.__check_pairs(u_find, [(9, 10)])
+
+        self.__merge_pairs(u_find, [(1, 5), (2, 6)])
+        self.assertEqual(len(u_find), 3)
+        self.__check_pairs(u_find, [(1, 5), (2, 6)])
+
+        self.__merge_pairs(u_find, [(7, 3), (8, 4)])
+        self.assertEqual(len(u_find), 3)
+        self.__check_pairs(u_find, [(7, 3), (8, 4)])
+
+        self.__merge_pairs(u_find, [(7, 2)])
+        self.assertEqual(len(u_find), 2)
+        self.__check_pairs(u_find, [(7, 2)])
+
+        self.__merge_pairs(u_find, [(8, 1)])
+        self.assertEqual(len(u_find), 2)
+        self.__check_pairs(u_find, [(8, 1)])
+
+        self.__merge_pairs(u_find, [(10, 2)])
+        self.assertEqual(len(u_find), 1)
+        self.__check_pairs(u_find, [(10, 2)])
+
+    def __add_elems(self, u_find, data):
+
+        starting_size = len(u_find)
+
+        for elem in data: 
+            u_find.add_element(elem)
+
+        self.assertEqual(len(u_find) - starting_size, len(data))
+
+        working_values = [pair for pair in u_find]
+        self.assertEqual([pair[0] for pair in working_values[starting_size:]], data)
+
+    def __set_elems(self, u_find, data_pairs):
+
+        for pair in data_pairs:
+            u_find.set_element(*pair)
+        
+        working_values = [pair for pair in u_find]
+        for pair in data_pairs:
+            self.assertEqual(working_values[pair[0] - 1][0], pair[1])
+
+    def __merge_into_group(self, u_find, indices):
+
+        initial_groups = len(u_find)
+        group_reference = sorted(indices)[0]
+
+        for index in indices:
+            u_find.merge(index, group_reference)
+
+        self.assertEqual(initial_groups - len(u_find), len(indices) - 1)
+
+        working_values = [pair for pair in u_find]
+        for index in indices:
+            self.assertEqual(working_values[index - 1][1], group_reference)
+
+    def __merge_pairs(self, u_find, index_pairs):
+
+        for pair in index_pairs:
+            u_find.merge(*pair)    
+
+    def __check_if_elems_in_group(self, u_find, indices) -> bool:
+        
+        group_reference = sorted(indices)[0]
+
+        for index in indices:
+            if not u_find.is_same_group(index, group_reference):
+                return False
+
+        return True
+
+    def __check_pairs(self, u_find, index_pairs):
+
+        for pair in index_pairs:
+            self.assertEqual(u_find.is_same_group(*pair), True)    
+
 
 if __name__ == "__main__":
     unittest.main()
